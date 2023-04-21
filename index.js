7fd91e660ccbe36ea52335b19e157bc20e9208f8
Sinny <76957008+SinnyNo1@users.noreply.github.com>
2023-04-21 23:55:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let host_client
 // EXPRESS \\ 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
     var q = req.query
